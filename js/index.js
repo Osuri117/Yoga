60d77cdc4cbfd81a7ab9595f6af7eeda10b11293
Maxence <beirnaert.max@gmail.com>
2023-05-17 16:57:10 +0200

    creation de mes bouton et tapis afin de commencer a branche le js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const date1 = document.querySelector("#d1")
+const date2 = document.querySelector("#d2")
+const date3 = document.querySelector("#d3")
+
+const tabTapis = document.querySelectorAll("div.tapis")
+
+
+
+console.log(date1.value, tabTapis)
